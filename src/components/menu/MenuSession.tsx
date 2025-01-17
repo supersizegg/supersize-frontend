8d5b3be04418cddf25dfe07b939cca42e3f6bdb0
dwrx <dwrx@miso.one>
2025-01-17 13:12:24 +0100

    Lock session wallet buttons when processing TX

--- src/components/menu/MenuSession.tsx
+++ src/components/menu/MenuSession.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useMagicBlockEngine } from "../../engine/MagicBlockEngineProvider";
+import { Spinner } from "../util/Spinner";
 import "./MenuSession.scss";
 
 export function MenuSession() {
@@ -7,12 +8,14 @@ export function MenuSession() {
 
   const sessionPayer = engine.getSessionPayer();
   const [sessionLamports, setSessionLamports] = React.useState<number | undefined>(undefined);
+  const [isFunding, setIsFunding] = React.useState(false);
+  const [isWithdrawing, setIsWithdrawing] = React.useState(false);
 
   React.useEffect(() => {
     return engine.subscribeToChainAccountInfo(sessionPayer, (accountInfo) => {
       setSessionLamports(accountInfo?.lamports);
     });
-  }, [engine]);
+  }, [engine, sessionPayer]);
 
   const solBalance = sessionLamports !== undefined ? (sessionLamports / 1_000_000_000).toFixed(3) : "0";
 
@@ -20,19 +23,25 @@ export function MenuSession() {
 
   const onFund = async () => {
     try {
+      setIsFunding(true);
       await engine.fundSessionFromWallet();
       console.log("Session funded from wallet");
     } catch (err) {
       console.error("Funding error:", err);
+    } finally {
+      setIsFunding(false);
     }
   };
 
   const onWithdraw = async () => {
     try {
+      setIsWithdrawing(true);
       await engine.defundSessionBackToWallet();
       console.log("Session funds withdrawn");
     } catch (err) {
       console.error("Withdraw error:", err);
+    } finally {
+      setIsWithdrawing(false);
     }
   };
 
@@ -48,13 +57,13 @@ export function MenuSession() {
 
         <div className="session-buttons">
           {needsFunding && (
-            <button className="btn-fund" onClick={onFund}>
-              Fund
+            <button className="btn-fund" onClick={onFund} disabled={isFunding}>
+              {isFunding ? <Spinner /> : "Fund"}
             </button>
           )}
 
-          <button className="btn-withdraw" onClick={onWithdraw}>
-            Withdraw
+          <button className="btn-withdraw" onClick={onWithdraw} disabled={isWithdrawing}>
+            {isWithdrawing ? <Spinner /> : "Withdraw"}
           </button>
         </div>
       </div>
---
+++ src/components/util/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Spinner.scss";
+
+export function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="spinner-circle"></div>
+    </div>
+  );
+}
