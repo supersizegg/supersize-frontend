1c42f3b940c633637884ccf0040a3f6379fa7edf
dwrx <dwrx@miso.one>
2025-02-09 10:00:19 -0700

    Refactored Create Game

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { API_BASE_URL, cachedTokenMetadata, endpoints, NETWORK } from "@utils/constants";
 import { Blob } from "@utils/types";
 
@@ -12,6 +13,56 @@ import { COMPONENT_PLAYER_ID } from "@states/gamePrograms";
 import { FindComponentPda } from "@magicblock-labs/bolt-sdk";
 import { BN } from "@coral-xyz/anchor";
 import { HELIUS_API_KEY } from "@utils/constants";
+
+// UI helpers
+export async function addTransaction(
+  setTransactions: React.Dispatch<React.SetStateAction<{ id: string; status: string }[]>>,
+  id: string,
+  status: string,
+) {
+  setTransactions((prev) => [...prev, { id, status }]);
+}
+
+export async function updateTransaction(
+  setTransactions: React.Dispatch<React.SetStateAction<{ id: string; status: string }[]>>,
+  id: string,
+  status: string,
+) {
+  setTransactions((prev) => prev.map((txn) => (txn.id === id ? { ...txn, status } : txn)));
+}
+
+export async function retryTransaction(
+  transactionId: string,
+  transactionFn: () => Promise<void>,
+  setTransactions: React.Dispatch<React.SetStateAction<{ id: string; status: string }[]>>,
+  showPrompt: (errorMessage: string) => Promise<boolean>,
+) {
+  let retry = true;
+  while (retry) {
+    try {
+      await transactionFn();
+      await updateTransaction(setTransactions, transactionId, "confirmed");
+      retry = false;
+    } catch (error) {
+      await updateTransaction(setTransactions, transactionId, "failed");
+      let message = "Unknown Error";
+      if (error instanceof Error) {
+        message = error.message;
+      }
+      retry = await showPrompt(`Transaction ${transactionId} failed: ${JSON.stringify(message)}`);
+    }
+  }
+}
+
+export async function executeStep(
+  stepId: string,
+  stepFn: () => Promise<void>,
+  setTransactions: React.Dispatch<React.SetStateAction<{ id: string; status: string }[]>>,
+  showPrompt: (errorMessage: string) => Promise<boolean>,
+) {
+  await addTransaction(setTransactions, stepId, "pending");
+  await retryTransaction(stepId, stepFn, setTransactions, showPrompt);
+}
 
 export const stringToUint8Array = (str: string): Uint8Array => {
   return new TextEncoder().encode(str);
